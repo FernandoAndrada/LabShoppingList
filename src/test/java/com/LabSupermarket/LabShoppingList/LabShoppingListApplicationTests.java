f1f8355c042c29dcdf2917e2cd323898fed3c2b4
FernandoAndrada <fernandojcandrada@gmail.com>
2022-11-28 22:05:33 -0300

    first commit

---
+++ src/test/java/com/LabSupermarket/LabShoppingList/LabShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LabSupermarket.LabShoppingList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabShoppingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
