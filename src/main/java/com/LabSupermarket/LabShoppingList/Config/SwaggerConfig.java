9a001bdb424afc09b48d662e582445483bc9fd7e
FernandoAndrada <fernandojcandrada@gmail.com>
2022-11-30 11:57:11 -0300

    Criar branch swagger e swagger (problemas).

---
+++ src/main/java/com/LabSupermarket/LabShoppingList/Config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.LabSupermarket.LabShoppingList.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public Docket apiDocket(){
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.any()).build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("index.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+    }
+}
