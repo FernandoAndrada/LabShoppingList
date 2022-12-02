77adf1070e2c45c93d17ed7ac063a8a1a19972de
FernandoAndrada <fernandojcandrada@gmail.com>
2022-12-02 14:40:49 -0300

    Commit de finalização, retirando error-controller do Swagger.

--- src/main/java/com/LabSupermarket/LabShoppingList/api/error/ApiExceptionError.java
+++ src/main/java/com/LabSupermarket/LabShoppingList/api/error/ApiExceptionError.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
 
 
     @ControllerAdvice
+    @ApiIgnore
     public class ApiExceptionError extends ResponseEntityExceptionHandler {
 
         @ExceptionHandler(EntityNotFoundException.class)
